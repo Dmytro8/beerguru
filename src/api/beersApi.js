7fe8138558e99461a5d06bd0273ab61963893c4b
Dmytro Patalakha <dm.patalakha@gmail.com>
2020-03-13 15:37:41 +0100

    create beer API client

--- src/api/beersApi.js
+++ src/api/beersApi.js
@@ -5,11 +5,31 @@ const instance = axios.create({
 });
 
 export const beersApi = {
-  getBeers(page, perPage) {
-    return instance
-      .get(`/beers?page=${page}&per_page=${perPage}`)
-      .then(response => {
-        return response.data;
-      });
+  async getBeers(page, perPage) {
+    const response = await instance.get(
+      `/beers?page=${page}&per_page=${perPage}`
+    );
+    return response.data;
+  },
+  async getAllBeers() {
+    let beersData = [];
+    for (let i = 1; i < 6; i++) {
+      const response = await instance.get(`/beers?page=${i}&per_page=80`);
+      beersData = beersData.concat(response.data);
+    }
+    return beersData;
+  },
+  async getBeer(beerId) {
+    const response = await instance.get(`/beers/${beerId}`);
+    return response.data;
+  },
+  async getSimilarBeers(ibu_gt, ibu_lt, abv_gt, abv_lt, ebc_gt, ebc_lt) {
+    let compareIBU = `ibu_gt=${ibu_gt}&ibu_lt${ibu_lt}`;
+    let compareABV = `abv_gt=${abv_gt}&abv_lt=${abv_lt}`;
+    let compareEBC = `ebc_gt=${ebc_gt}&ebc_lt=${ebc_lt}`;
+    const response = await instance.get(
+      `beers?${compareIBU}&${compareABV}&${compareEBC}`
+    );
+    return response.data;
   }
 };
