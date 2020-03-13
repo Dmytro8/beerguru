eb0f23f49b11c2b68dc0aec1c05527a2c7e8f113
Dmytro Patalakha <dm.patalakha@gmail.com>
2020-03-13 15:39:29 +0100

    modal window with beer details was done

---
+++ src/components/BeerDetails/BeerDetails.js
@@ -0,0 +1,156 @@
+import React, { useState, useEffect } from "react";
+
+import classes from "./BeerDetails.module.scss";
+import { useHistory, Link } from "react-router-dom";
+import { useParams } from "react-router";
+import { Spinner } from "../Spinner";
+import { beersApi } from "../../api/beersApi";
+import { Beer } from "../Beer/Beer";
+import { findSimilarBeers } from "../../utils/comparing";
+
+export const BeerDetails = ({ allBeers, statusModal, setStatusModal }) => {
+  let { beerId } = useParams();
+  const history = useHistory();
+  const [isDataFetched, setIsDataFetched] = useState(false);
+  const [beer, setBeer] = useState(undefined);
+  const [isRecBeersFetched, setisRecBeersFetched] = useState(false);
+  const [recBeersArrayOfId, setRecBeersArrayOfId] = useState([]);
+
+  useEffect(() => {
+    document.body.classList.add("modal-open");
+    async function fetchData() {
+      const fetchedBeer = await beersApi.getBeer(beerId);
+      setBeer(fetchedBeer[0]);
+      setIsDataFetched(true);
+      const fetchSimilarBeer = async (
+        ibu,
+        abv,
+        ebc,
+        ibuArea,
+        abvArea,
+        ebcArea
+      ) => {
+        const fetchedSimilarBeer = await beersApi.getSimilarBeers(
+          (ibu - ibuArea).toFixed(1),
+          (ibu + ibuArea).toFixed(1),
+          (abv - abvArea).toFixed(1),
+          (abv + abvArea).toFixed(1),
+          (ebc - ebcArea).toFixed(1),
+          (ebc + ebcArea).toFixed(1)
+        );
+        return fetchedSimilarBeer;
+      };
+      findSimilarBeers(
+        fetchedBeer[0],
+        fetchSimilarBeer,
+        setRecBeersArrayOfId,
+        setisRecBeersFetched
+      );
+    }
+    fetchData();
+    return () => {
+      document.body.classList.remove("modal-open");
+      setBeer(null);
+      setIsDataFetched(false);
+    };
+  }, [beerId]);
+
+  function closeModal() {
+    history.push("/");
+    setStatusModal("close");
+  }
+
+  return (
+    <div className={`${statusModal === "open" ? "visible" : "hide"}`}>
+      <div className={classes.overlay} onClick={closeModal}></div>
+      <div className={classes.modal}>
+        <div className={classes.beerDetails}>
+          {isDataFetched ? (
+            <div className={classes.details}>
+              <div className={classes.beerImgWrapper}>
+                <img src={beer.image_url} alt="beer" />
+              </div>
+              <div className={classes.beerDetailsWrapper}>
+                <div className={classes.beerName}>
+                  <h2>{beer.name}</h2>
+                </div>
+                <div className={classes.beerTagline}>
+                  <h3>{beer.tagline}</h3>
+                </div>
+                <div className={classes.separatorLine}>
+                  <span></span>
+                </div>
+                <div className={classes.beerStandards}>
+                  <h3>
+                    <span>Ibu:</span>
+                    {beer.ibu}
+                  </h3>
+                  <h3>
+                    <span>Abv:</span>
+                    {beer.abv}%
+                  </h3>
+                  <h3>
+                    <span>Ebc:</span>
+                    {beer.ebc}
+                  </h3>
+                </div>
+                <div className={classes.beerDescription}>
+                  <p>{beer.description}</p>
+                </div>
+                <div className={classes.foodPairing}>
+                  <h3>Best served with:</h3>
+                  <ul>
+                    {beer.food_pairing.map((e, index) => (
+                      <li key={index}>- {e}</li>
+                    ))}
+                  </ul>
+                </div>
+              </div>
+            </div>
+          ) : (
+            <div className={classes.spinnerBeerWrapper}>
+              <Spinner />
+            </div>
+          )}
+          <div className={classes.recommendation}>
+            <div>
+              <h3>You might also like:</h3>
+            </div>
+            {isRecBeersFetched ? (
+              <div className={classes.recommendedBeers}>
+                <div className={classes.beersWrapper}>
+                  {allBeers
+                    .filter(beer => {
+                      return (
+                        beer.id === recBeersArrayOfId[0] ||
+                        beer.id === recBeersArrayOfId[1] ||
+                        beer.id === recBeersArrayOfId[2]
+                      );
+                    })
+                    .map(beer => (
+                      <Link key={beer.id} to={`/details/${beer.id}`}>
+                        <Beer
+                          imageUrl={beer.image_url}
+                          beerName={beer.name}
+                          isInModal={true}
+                        />
+                      </Link>
+                    ))}
+                </div>
+              </div>
+            ) : (
+              <div className={classes.spinner}>
+                <Spinner />
+              </div>
+            )}
+          </div>
+          <div className={classes.buttonCloseModal}>
+            <button onClick={closeModal}>
+              <i className="fas fa-times-circle"></i>
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
